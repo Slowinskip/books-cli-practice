4342014d50bc1982bf8e0ecafa82825d45a0211a
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-01-26 11:17:22 +0100

    Add endpoint to get all author and unique author

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Param } from '@nestjs/common/decorators';
+import { ParseUUIDPipe } from '@nestjs/common/pipes';
+import { AuthorsService } from './authors.service';
 
 @Controller('authors')
-export class AuthorsController {}
+export class AuthorsController {
+  constructor(private authorsService: AuthorsService) {}
+
+  @Get('/')
+  getAll() {
+    return this.authorsService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const author = await this.authorsService.getById(id);
+    if (!author) throw new Error('Author not found');
+    return author;
+  }
+}
--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/shared/services/prisma.service';
 import { AuthorsController } from './authors.controller';
 import { AuthorsService } from './authors.service';
 
 @Module({
   controllers: [AuthorsController],
-  providers: [AuthorsService]
+  providers: [AuthorsService, PrismaService],
 })
 export class AuthorsModule {}
