01319bf86ddbe439031fbb96f92547973610b30e
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-01-26 12:24:40 +0100

    Add module book and create endpoints

---
+++ src/books/books.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  NotFoundException,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { Param } from '@nestjs/common/decorators';
+import { ParseUUIDPipe } from '@nestjs/common/pipes';
+
+import { BooksService } from './books.service';
+import { CreateBookDTO } from './dtos/create-book.dto';
+import { UpdateBookDTO } from './dtos/update-book.dto';
+
+@Controller('books')
+export class BooksController {
+  constructor(private bookService: BooksService) {}
+
+  @Get('/')
+  getAll() {
+    return this.bookService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const book = await this.bookService.getById(id);
+    if (!book) throw new Error('Book not found');
+    return book;
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.bookService.getById(id)))
+      throw new NotFoundException('Book not found');
+    await this.bookService.delete(id);
+    return { success: true };
+  }
+
+  @Post('/')
+  public create(@Body() bookData: CreateBookDTO) {
+    return this.bookService.create(bookData);
+  }
+
+  @Put('/:id')
+  async updateById(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() bookData: UpdateBookDTO,
+  ) {
+    if (!(await this.bookService.getById(id)))
+      throw new NotFoundException('Book not found');
+    await this.bookService.updateById(id, bookData);
+    return { success: true };
+  }
+}
---
+++ src/books/books.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/shared/services/prisma.service';
+import { Book } from '@prisma/client';
+import { ConflictException } from '@nestjs/common/exceptions';
+
+@Injectable()
+export class BooksService {
+  constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Book[]> {
+    return this.prismaService.book.findMany();
+  }
+
+  public getById(id: Book['id']): Promise<Book | null> {
+    return this.prismaService.book.findUnique({
+      where: { id },
+    });
+  }
+
+  public delete(id: Book['id']): Promise<Book> {
+    return this.prismaService.book.delete({
+      where: { id },
+    });
+  }
+
+  public create(
+    bookData: Omit<Book, 'id' | 'createdAt' | 'updatedAt'>,
+  ): Promise<Book> {
+    const { authorId, ...otherData } = bookData;
+    try {
+      return this.prismaService.book.create({
+        data: {
+          ...otherData,
+          author: {
+            connect: { id: authorId },
+          },
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new ConflictException('This book already exists');
+      throw error;
+    }
+  }
+
+  public updateById(
+    id: Book['id'],
+    bookData: Omit<Book, 'id' | 'createdAt' | 'updatedAt'>,
+  ): Promise<Book> {
+    return this.prismaService.book.update({
+      where: { id },
+      data: bookData,
+    });
+  }
+}
